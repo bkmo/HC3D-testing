f58a3a1b753a2b9b7fc1e2cf5304a73df25bebe7
bkmo <bkmo98@gmail.com>
2011-06-17 12:56:06 -0600

    new drawables from Mike

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := hc3d-test1
+LOCAL_PACKAGE_NAME := hc3d-test2
 LOCAL_AAPT_FLAGS := -c hdpi,mdpi -x 10
 LOCAL_CERTIFICATE := bkmo98
 
